2299e9d6d44a1d3ffd75a1440a599b7ff3a50b68
Adnan <adnan.b.hasan@gmail.com>
2023-03-17 14:55:35 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,14 @@
 #include <unistd.h>
 
 /**
-* main - print quote
-*
-* Description: print Dora's quote
-* Return: Always 1 (success)
-*/
-
+ * main - print quote
+ *
+ * Description: prints Dora korpar's quote
+ *
+ * Return: Always 1 (Success)
+ */
 int main(void)
-
- {
- write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
- return (1);
- }
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
