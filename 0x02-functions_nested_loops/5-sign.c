004e07a4cb2e6971729d5ade8664917c34062899
Adnan <adnan.b.hasan@gmail.com>
2023-03-23 06:47:03 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,10 +2,10 @@
 
 /**
  * print_sign - prints the sign of a number
- * @c: the int check
+ * @n: the int to check
  * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
- * -1 ad prints - if n is less than zero
+ * -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
