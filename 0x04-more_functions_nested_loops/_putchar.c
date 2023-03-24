5c0cbfc64ee00ab3507a1702db0dfcde5adc3c80
Adnan <adnan.b.hasan@gmail.com>
2023-03-23 18:18:44 -0700

    all tasks

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -5,8 +5,9 @@
  * @c: The character to print
  *
  * Return: on success 1.
- * on error, -1 is returned, ad errno is set approriately.
+ * on error, -1 is returned, and errno is set approriately.
  */
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
