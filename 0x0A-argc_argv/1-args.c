5026ac74c91a2a8b3f00fe0fb0e0983eda748b7e
Adnan <adnan.b.hasan@gmail.com>
2023-04-08 10:14:22 -0700

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,19 +5,12 @@
 
 
 /**
-
  * main - print the number of arguments passed to the program
-
  * @argc: number of arguments
-
  * @argv: array of arguments
-
  *
-
  * Return: Always 0 (Success)
-
  */
-
 
 
 int main(int argc, char *argv[])
@@ -33,4 +26,3 @@ int main(int argc, char *argv[])
 	return (0);
 
 }
-
